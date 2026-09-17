733d5d9f4e476242f1cd99584815083711e0d1a9
Grados Rawk <gradosus@gmail.com>
2026-09-17 13:53:52 +0800

    Make client profiles robust when map address is unavailable

--- Model/Client.cs
+++ Model/Client.cs
@@ -10,7 +10,6 @@ using System.Net.Mail;
 
 namespace _4RTools.Model
 {
-
     public class ClientDTO
     {
         public int index { get; set; }
@@ -18,9 +17,7 @@ namespace _4RTools.Model
         public string description { get; set; }
         public string hpAddress { get; set; }
         public string nameAddress { get; set; }
-
         public string mapAddress { get; set; }
-
         public int hpAddressPointer { get; set; }
         public int nameAddressPointer { get; set; }
         public int mapAddressPointer { get; set; }
@@ -29,69 +26,48 @@ namespace _4RTools.Model
 
         public ClientDTO(string name, string description, string hpAddress, string nameAddress, string mapAddress)
         {
-            this.name= name;
+            this.name = name;
             this.description = description;
             this.hpAddress = hpAddress;
             this.nameAddress = nameAddress;
             this.mapAddress = mapAddress;
-
-            this.hpAddressPointer = Convert.ToInt32(hpAddress, 16);
-            this.nameAddressPointer = Convert.ToInt32(nameAddress, 16);
-            this.mapAddressPointer = Convert.ToInt32(mapAddress, 16);
-
+            this.hpAddressPointer = ParseAddress(hpAddress);
+            this.nameAddressPointer = ParseAddress(nameAddress);
+            this.mapAddressPointer = ParseAddress(mapAddress);
         }
 
+        private static int ParseAddress(string address)
+        {
+            if (string.IsNullOrWhiteSpace(address)) return 0;
+            return Convert.ToInt32(address, 16);
+        }
     }
-
 
     public sealed class ClientListSingleton
     {
         private static List<Client> clients = new List<Client>();
-        
-        public static void AddClient(Client c)
-        {
-            clients.Add(c);
-        }
 
-        public static void RemoveClient(Client c)
-        {
-            clients.Remove(c);
-        }
-
-        public static List<Client> GetAll()
-        {
-            return clients;
-        }
+        public static void AddClient(Client c) { clients.Add(c); }
+        public static void RemoveClient(Client c) { clients.Remove(c); }
+        public static List<Client> GetAll() { return clients; }
 
         public static bool ExistsByProcessName(string processName)
         {
-            return clients.Exists(client => client.processName == processName);
+            return clients.Exists(client => string.Equals(client.processName, processName, StringComparison.OrdinalIgnoreCase));
         }
     }
 
     public sealed class ClientSingleton
     {
         private static Client client;
-        private ClientSingleton(Client client)
-        {
-            ClientSingleton.client = client;
-        }
-
-        public static ClientSingleton Instance(Client client)
-        {
-            return new ClientSingleton(client);
-        }
-
-        public static Client GetClient()
-        {
-            return client;
-        }
+        private ClientSingleton(Client client) { ClientSingleton.client = client; }
+        public static ClientSingleton Instance(Client client) { return new ClientSingleton(client); }
+        public static Client GetClient() { return client; }
     }
 
     public class Client
     {
         public Process process { get; }
-
         public string processName { get; private set; }
         private Utils.ProcessMemoryReader PMR { get; set; }
         public int currentNameAddress { get; set; }
@@ -114,18 +90,25 @@ namespace _4RTools.Model
         public Client(ClientDTO dto)
         {
             this.processName = dto.name;
-            this.currentHPBaseAddress = Convert.ToInt32(dto.hpAddress, 16);
-            this.currentNameAddress = Convert.ToInt32(dto.nameAddress, 16);
-            this.currentMapAddress = Convert.ToInt32(dto.mapAddress, 16);
+            this.currentHPBaseAddress = ParseAddress(dto.hpAddress);
+            this.currentNameAddress = ParseAddress(dto.nameAddress);
+            this.currentMapAddress = ParseAddress(dto.mapAddress);
             this.statusBufferAddress = this.currentHPBaseAddress + 0x474;
             this.currentOpenChatAddress = 0x012A714C;
+        }
+
+        private static int ParseAddress(string address)
+        {
+            if (string.IsNullOrWhiteSpace(address)) return 0;
+            return Convert.ToInt32(address, 16);
         }
 
         public Client(string processName)
         {
             PMR = new Utils.ProcessMemoryReader();
-            string rawProcessName = processName.Split(new string[] { ".exe - " }, StringSplitOptions.None)[0];
-            int choosenPID = int.Parse(processName.Split(new string[] { ".exe - " }, StringSplitOptions.None)[1]);
+            string[] parts = processName.Split(new string[] { ".exe - " }, StringSplitOptions.None);
+            string rawProcessName = parts[0];
+            int choosenPID = int.Parse(parts[1]);
 
             foreach (Process process in Process.GetProcessesByName(rawProcessName))
             {
@@ -138,9 +121,7 @@ namespace _4RTools.Model
                     try
                     {
                         Client c = GetClientByProcess(rawProcessName);
-
                         if (c == null) throw new Exception();
-
                         this.currentHPBaseAddress = c.currentHPBaseAddress;
                         this.currentNameAddress = c.currentNameAddress;
                         this.currentMapAddress = c.currentMapAddress;
@@ -149,120 +130,62 @@ namespace _4RTools.Model
                     }
                     catch
                     {
-                        MessageBox.Show("This client is not supported. Only Spammers and macro will works.", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                        MessageBox.Show("This client is not supported. Only spammers and macros will work.", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
                         this.currentHPBaseAddress = 0;
                         this.currentNameAddress = 0;
                         this.currentMapAddress = 0;
                         this.statusBufferAddress = 0;
                         this.currentOpenChatAddress = 0;
                     }
-                   
-                    //Do not block spammer for non supported Versions
-                       
                 }
             }
         }
 
         private string ReadMemoryAsString(int address)
         {
+            if (address == 0) return string.Empty;
             byte[] bytes = PMR.ReadProcessMemory((IntPtr)address, 40u, out _num);
-            List<byte> buffer = new List<byte>(); //Need a list with dynamic size 
-            for (int i =0;i < bytes.Length;i++)
+            List<byte> buffer = new List<byte>();
+            for (int i = 0; i < bytes.Length; i++)
             {
-                if (bytes[i] == 0) break; //Check Nullability based ON ASCII Table
-
-                buffer.Add(bytes[i]); //Add only bytes needed
+                if (bytes[i] == 0) break;
+                buffer.Add(bytes[i]);
             }
-
-           return Encoding.Default.GetString(buffer.ToArray());
-
+            return Encoding.Default.GetString(buffer.ToArray());
         }
 
         private uint ReadMemory(int address)
         {
+            if (address == 0) return 0;
             return BitConverter.ToUInt32(PMR.ReadProcessMemory((IntPtr)address, 4u, out _num), 0);
         }
 
         public byte ReadMemoryAsByte(int address)
         {
+            if (address == 0) return 0;
             byte[] bytes = PMR.ReadProcessMemory((IntPtr)address, 1, out _num);
-
             return bytes[0];
         }
-        public void WriteMemory(int address, uint intToWrite)
-        {
-            PMR.WriteProcessMemory((IntPtr)address, BitConverter.GetBytes(intToWrite), out _num);
-        }
-
-        public void WriteMemory(int address, byte[] bytesToWrite)
-        {
-            PMR.WriteProcessMemory((IntPtr)address, bytesToWrite, out _num);
-        }
-
-        public bool IsHpBelow(int percent)
-        {
-            return ReadCurrentHp() * 100 < percent * ReadMaxHp();
-        }
-
-        public bool IsSpBelow(int percent)
-        {
-            return ReadCurrentSp() * 100 < percent * ReadMaxSp();
-        }
-        public bool IsHpAbove(int percent)
-        {
-            return ReadCurrentHp() * 100 > percent * ReadMaxHp();
-        }
-
-        public bool IsSpAbove(int percent)
-        {
-            return ReadCurrentSp() * 100 > percent * ReadMaxSp();
-        }
-
-        public uint ReadCurrentHp()
-        {
-            return ReadMemory(this.currentHPBaseAddress);
-        }
-
-        public uint ReadCurrentSp()
-        {
-            return ReadMemory(this.currentHPBaseAddress + 8);
-        }
-
-        public uint ReadMaxHp()
-        {
-            return ReadMemory(this.currentHPBaseAddress + 4);
-        }
-
-        public string ReadCharacterName()
-        {
-            return ReadMemoryAsString(this.currentNameAddress);
-        }
-
-        public string ReadCurrentMap()
-        {
-            return ReadMemoryAsString(this.currentMapAddress);
-        }
-        public bool ReadOpenChat()
-        {
-            return Convert.ToBoolean(ReadMemoryAsByte(this.currentOpenChatAddress));
-        }
-
-        public uint ReadMaxSp()
-        {
-            return ReadMemory(this.currentHPBaseAddress + 12);
-        }
-
-        public uint CurrentBuffStatusCode(int effectStatusIndex)
-        {
-            return ReadMemory(this.statusBufferAddress + effectStatusIndex * 4);
-        }
+        public void WriteMemory(int address, uint intToWrite) { PMR.WriteProcessMemory((IntPtr)address, BitConverter.GetBytes(intToWrite), out _num); }
+        public void WriteMemory(int address, byte[] bytesToWrite) { PMR.WriteProcessMemory((IntPtr)address, bytesToWrite, out _num); }
+        public bool IsHpBelow(int percent) { return ReadCurrentHp() * 100 < percent * ReadMaxHp(); }
+        public bool IsSpBelow(int percent) { return ReadCurrentSp() * 100 < percent * ReadMaxSp(); }
+        public bool IsHpAbove(int percent) { return ReadCurrentHp() * 100 > percent * ReadMaxHp(); }
+        public bool IsSpAbove(int percent) { return ReadCurrentSp() * 100 > percent * ReadMaxSp(); }
+        public uint ReadCurrentHp() { return ReadMemory(this.currentHPBaseAddress); }
+        public uint ReadCurrentSp() { return ReadMemory(this.currentHPBaseAddress + 8); }
+        public uint ReadMaxHp() { return ReadMemory(this.currentHPBaseAddress + 4); }
+        public string ReadCharacterName() { return ReadMemoryAsString(this.currentNameAddress); }
+        public string ReadCurrentMap() { return ReadMemoryAsString(this.currentMapAddress); }
+        public bool ReadOpenChat() { return Convert.ToBoolean(ReadMemoryAsByte(this.currentOpenChatAddress)); }
+        public uint ReadMaxSp() { return ReadMemory(this.currentHPBaseAddress + 12); }
+        public uint CurrentBuffStatusCode(int effectStatusIndex) { return ReadMemory(this.statusBufferAddress + effectStatusIndex * 4); }
 
         public Client GetClientByProcess(string processName)
         {
-       
-            foreach(Client c in ClientListSingleton.GetAll())
+            foreach (Client c in ClientListSingleton.GetAll())
             {
-                if (c.processName == processName)
+                if (string.Equals(c.processName, processName, StringComparison.OrdinalIgnoreCase))
                 {
                     uint hpBaseValue = ReadMemory(c.currentHPBaseAddress);
                     if (hpBaseValue > 0) return c;
@@ -270,13 +193,14 @@ namespace _4RTools.Model
             }
             return null;
         }
-    
+
         public static Client FromDTO(ClientDTO dto)
         {
             return ClientListSingleton.GetAll()
-                .Where(c => c.processName == dto.name)
+                .Where(c => string.Equals(c.processName, dto.name, StringComparison.OrdinalIgnoreCase))
                 .Where(c => c.currentHPBaseAddress == dto.hpAddressPointer)
-                .Where(c => c.currentNameAddress == dto.nameAddressPointer).FirstOrDefault();
+                .Where(c => c.currentNameAddress == dto.nameAddressPointer)
+                .FirstOrDefault();
         }
     }
 }
